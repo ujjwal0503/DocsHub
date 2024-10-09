1a420a40f9f40089ae495100f1b4bfb8f311ad24
ujjwal0503 <154718955+ujjwal0503@users.noreply.github.com>
2024-10-09 22:56:25 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,13 +9,6 @@ var usersRouter = require('./routes/users');
 
 var cors = require("cors");
 var app = express();
-app.use(cors(
-  {
-    origin: ["https://docs-hub-api.vercel.app/"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
